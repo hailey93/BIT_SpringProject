2f61bfd00e7a73a1a670b24e2f9ce6354d24aaf0
YouHyeonJun <collin7202@gmail.com>
2020-04-03 18:02:51 +0900

    css 분리

--- src/main/java/com/bit/lib/controller/MainController.java
+++ src/main/java/com/bit/lib/controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bit.lib.dao.MainDAO;
@@ -20,8 +21,9 @@ public class MainController {
 		return "main";
 	}
 	
+	
 	@GetMapping("/mainSearch")
-	public String mainSearch(Model model, @RequestParam("keyWord") String keyWord) {
+	public String mainSearch(Model model, @RequestParam("keyWord") String keyWord ) {
 		
 
 		model.addAttribute("keyWord", keyWord);
