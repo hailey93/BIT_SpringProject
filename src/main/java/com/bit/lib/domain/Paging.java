efe1e8273bb031379f0203665b3aef4e417af94c
YouHyeonJun <collin7202@gmail.com>
2020-04-08 20:20:54 +0900

    페이징 완료. 우승!

--- src/main/java/com/bit/lib/domain/Paging.java
+++ src/main/java/com/bit/lib/domain/Paging.java
@@ -16,17 +16,16 @@ public class Paging {
 	private int startList; //해당 페이지 목록 시작 번호
 	private double listCnt; // 검색한 목록 총 개수
 	private int page; //현재 페이지
+	private int range;
 	private int pageCnt; // 전체 페이지 개수
-	private int startPage; //검색 게시물 시작 번호
-	private int endPage; // 검색 게시물 마지막 번호
 	private boolean prev; // 이전 페이지 목록
-	private boolean next; // 다음 페이지 목록
-	
-	
+	private boolean next; // 다음 페이지 목록	
 	
 	public void pageInfo(int page,int range, int listCnt) {
 		
-		this.page = page + range;
+		this.page = page;
+		
+		this.range = range;		
 		
 		this.listCnt = listCnt; // 전체 게시물 갯수
 		
@@ -34,14 +33,8 @@ public class Paging {
 		this.pageCnt = (int)Math.ceil(this.listCnt/listSize);
 		
 		//페이지 목록 검색 번호
-		this.startList = (this.page - 1) * listSize;
+		this.startList = (page + range - 1) * listSize;
 		
-		//시작 목록 번호
-		this.startPage = (this.page - 1) * pageSize + 1 ;
-		
-		//마지막 목록 번호
-		this.endPage = (this.page - 1) * pageSize + 6;
-
 		//이전 버튼 상태
 		this.prev = range == 0 ? false : true;
 		
